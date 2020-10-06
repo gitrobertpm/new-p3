942fd6fdb4217d768334effd1b927f9e3bf114b1
robertpm <grobertpm@gmail.com>
2020-10-06 02:58:46 -0700

    Added Accessibility features

--- js/script.js
+++ js/script.js
@@ -16,9 +16,6 @@ nameInput.focus();
 const jobRoleMenu = document.querySelector('#title');
 const otherJobRoleInput = document.querySelector('#other-job-role');
 
-// Hide default option
-jobRoleMenu.children[0].hidden = true;
-
 // Hide 'Other' field 
 otherJobRoleInput.style.opacity = 0;
 otherJobRoleInput.style.display = 'none';
@@ -43,12 +40,10 @@ jobRoleMenu.addEventListener('change', e => {
 const designMenu = document.querySelector('#design');
 const colorMenu = document.querySelector('#color');
 
-// Hide default option
-designMenu.children[0].hidden = true;
-
 // Create and display temporary placeholder option for color menu
-const placeholderOption = `<option selected hidden>Select a design theme</option>`;
+const placeholderOption = `<option selected hidden>Select a design theme above</option>`;
 colorMenu.insertAdjacentHTML('afterbegin', placeholderOption);
+
 
 // Helper function for hiding all color options
 const hideColors = () => [...colorMenu.children].forEach(option => option.hidden = true);
@@ -75,12 +70,22 @@ designMenu.addEventListener('change', e => {
  */
 const activitySection = document.querySelector('#activities');
 const activities = document.querySelectorAll('.activities input');
+const activityHint = document.querySelector('#activity-hint');
 
-// Add element to display cost
-const totalCostElement = `<h3 id="activity-cost"></h3>`;
-activitySection.insertAdjacentHTML('beforeend', totalCostElement);
-const activityTotalCost = document.querySelector('#activity-cost');
 let total = 0;
+
+
+// Accessibility - add tab index indicator to activity labels
+[...activities].forEach((activity) => {
+  activity.addEventListener('focus', e => {
+    activity.parentElement.classList.add('focus');
+  })
+
+  activity.addEventListener('blur', e => {
+    const active = document.querySelector('.focus');
+    if (active) active.classList.remove('focus');
+  })
+});
 
 // Update total cost and disable/enable conflicting options
 activitySection.addEventListener('change', e => {
@@ -93,7 +98,7 @@ activitySection.addEventListener('change', e => {
 
   // Update total cost
   clicked.checked ? total += cost : total -= cost;
-  activityTotalCost.innerHTML = `Total: $${total}`;
+  activityHint.innerHTML = `Total: $${total}`;
 
   // Dat and time of clicked activity
   const dayAndTime = clicked.getAttribute('data-day-and-time');
@@ -102,8 +107,13 @@ activitySection.addEventListener('change', e => {
   for (let i = 0; i < activities.length; i++ ) {
     const dayAndTimeToCheck = activities[i].getAttribute('data-day-and-time');
     if (dayAndTime === dayAndTimeToCheck && clicked !== activities[i]) {
-      clicked.checked ? activities[i].disabled = true : activities[i].disabled = false;
-      clicked.checked ? activities[i].parentElement.style.background = 'rgba(197, 163, 175, 0.1)' : activities[i].parentElement.style.background = 'white';
+      clicked.checked ? 
+      activities[i].disabled = true : 
+      activities[i].disabled = false;
+
+      clicked.checked ? 
+      activities[i].parentElement.style.background = 'rgba(197, 163, 175, 0.1)' : 
+      activities[i].parentElement.style.background = 'rgb(255, 253, 249)';
     }
   }
 });
@@ -123,12 +133,11 @@ paypal.style.opacity = 0;
 bitcoin.style.display = 'none';
 bitcoin.style.opacity = 0;
 
-// Hide default option
-paymentMethod.children[0].hidden = true;
-
 // Select credit card by default
 paymentMethod.children[1].setAttribute('selected', true);
 
+
+// Add fade in/out effect to payment sections
 const faeOut = (el) => {
   el.style.opacity = 0;
   setTimeout(e => { el.style.display = 'none' }, 350);
@@ -155,17 +164,42 @@ paymentMethod.addEventListener('change', e => {
  * Validation
  */
 const name = document.querySelector('#name');
-const mail = document.querySelector('#mail');
+const nameLabel = document.querySelector('label[for="name"]');
+const nameHint = document.querySelector('#name-hint');
+const email = document.querySelector('#email');
+const emailLabel = document.querySelector('label[for="email"]');
+const emailHint = document.querySelector('#email-hint');
 const activityLabels = document.querySelector('#activity-labels')
+const activityLegend = document.querySelector('#activities legend')
 const ccNum = document.querySelector('#cc-num');
+const ccLabel = document.querySelector('label[for="cc-num"]');
+const ccHint = document.querySelector('#cc-hint');
 const zip = document.querySelector('#zip');
+const zipLabel = document.querySelector('label[for="zip"]');
+const zipHint = document.querySelector('#zip-hint');
 const cvv = document.querySelector('#cvv');
+const cvvLabel = document.querySelector('label[for="cvv"]');
+const cvvHint = document.querySelector('#cvv-hint');
 
 
 // Name validation and real time test
 const validName = () => {
   const valid = name.value.length > 0;
-  valid ? name.style.borderColor = 'rgba(36, 28, 21, 0.3)' : name.style.borderColor = 'red';
+  if (valid) {
+    name.style.borderColor = 'rgba(36, 28, 21, 0.3)';
+    nameLabel.style.color = "rgb(0, 0, 0)";
+    nameLabel.classList.add('valid');
+    nameLabel.classList.remove('not-valid');
+    nameHint.style.color = 'rgba(0, 0, 0, 0.8)';
+    nameHint.textContent = 'Valid name';
+  } else {
+    name.style.borderColor = 'red';
+    nameLabel.style.color = "red";
+    nameLabel.classList.remove('valid');
+    nameLabel.classList.add('not-valid');
+    nameHint.style.color = 'red';
+    nameHint.textContent = 'Name field cannot be blank';
+  }
   return valid;
 }
 
@@ -176,17 +210,33 @@ name.addEventListener('keyup', e => {
 
 // Mail validation and real time test
 const validMail = () => {
-  const val = mail.value;
+  const val = email.value;
   const valid = val.length > 5 &&
                 val.includes('@') &&
                 val.includes('.') &&
                 val.indexOf('@') > 0 &&
                 val.indexOf('@') + 1 < val.lastIndexOf('.');
-  valid ? mail.style.borderColor = 'rgba(36, 28, 21, 0.3)' : mail.style.borderColor = 'red';
+
+  if (valid) {
+    email.style.borderColor = 'rgba(36, 28, 21, 0.3)';
+    emailLabel.style.color = "rgb(0, 0, 0)";
+    emailLabel.classList.add('valid');
+    emailLabel.classList.remove('not-valid');
+    emailHint.style.color = 'rgba(0, 0, 0, 0.8)';
+    emailHint.textContent = 'Valid email';
+  } else {
+    email.style.borderColor = 'red';
+    emailLabel.style.color = "red";
+    emailLabel.classList.remove('valid');
+    emailLabel.classList.add('not-valid');
+    emailHint.style.color = 'red';
+    emailHint.textContent = 'Email address must be formatted correctly';
+  }
+  // valid ? email.style.borderColor = 'rgba(36, 28, 21, 0.3)' : email.style.borderColor = 'red';
   return valid;
 }
 
-mail.addEventListener('keyup', e => {
+email.addEventListener('keyup', e => {
   validMail();
 })
 
@@ -194,7 +244,22 @@ mail.addEventListener('keyup', e => {
 // Activity validation and real time test
 const validActivity = () => {
   const valid = total > 0;
-  valid ? activityLabels.style.borderColor = 'transparent' : activityLabels.style.borderColor = 'red';
+  if (valid) {
+    activityLabels.style.borderColor = 'transparent';
+    activityLegend.style.color = "rgb(0, 0, 0)";
+    activityLegend.classList.add('valid');
+    activityLegend.classList.remove('not-valid');
+    activityHint.style.color = 'rgba(0, 0, 0, 0.8)';
+    activityHint.textContent = 'Activity selected';
+  } else {
+    activityLabels.style.borderColor = 'red';
+    activityLegend.style.color = "red";
+    activityLegend.classList.remove('valid');
+    activityLegend.classList.add('not-valid');
+    activityHint.style.color = 'red';
+    activityHint.textContent = 'Selecting at least one activity is required';
+  }
+  // valid ? activityLabels.style.borderColor = 'transparent' : activityLabels.style.borderColor = 'red';
   return valid;
 }
 
@@ -209,7 +274,22 @@ const validCC = () => {
   const valid = val.length > 12 &&
                 val.length < 17 &&
                 !isNaN(val); console.log(val.length);
-  valid ? ccNum.style.borderColor = 'rgba(36, 28, 21, 0.3)' : ccNum.style.borderColor = 'red';
+  // valid ? ccNum.style.borderColor = 'rgba(36, 28, 21, 0.3)' : ccNum.style.borderColor = 'red';
+  if (valid) {
+    ccNum.style.borderColor = 'rgba(36, 28, 21, 0.3)';
+    ccLabel.style.color = "rgb(0, 0, 0)";
+    ccLabel.classList.add('valid');
+    ccLabel.classList.remove('not-valid');
+    ccHint.style.color = 'rgba(0, 0, 0, 0.8)';
+    ccHint.textContent = 'Valid credit card number';
+  } else {
+    ccNum.style.borderColor = 'red';
+    ccLabel.style.color = "red";
+    ccLabel.classList.remove('valid');
+    ccLabel.classList.add('not-valid');
+    ccHint.style.color = 'red';
+    ccHint.textContent = 'Credit card number must be between 13 - 16 digits';
+  }
   return valid;
 }
 
@@ -223,7 +303,22 @@ const validZip = () => {
   const val = zip.value;
   const valid = val.length === 5 &&
                 !isNaN(val);
-  valid ? zip.style.borderColor = 'rgba(36, 28, 21, 0.3)' : zip.style.borderColor = 'red';
+  // valid ? zip.style.borderColor = 'rgba(36, 28, 21, 0.3)' : zip.style.borderColor = 'red';
+  if (valid) {
+    zip.style.borderColor = 'rgba(36, 28, 21, 0.3)';
+    zipLabel.style.color = "rgb(0, 0, 0)";
+    zipLabel.classList.add('valid');
+    zipLabel.classList.remove('not-valid');
+    zipHint.style.color = 'rgba(0, 0, 0, 0.8)';
+    zipHint.textContent = 'Valid Zip';
+  } else {
+    zip.style.borderColor = 'red';
+    zipLabel.style.color = "red";
+    zipLabel.classList.remove('valid');
+    zipLabel.classList.add('not-valid');
+    zipHint.style.color = 'red';
+    zipHint.textContent = 'Zip Code must be 5 digits';
+  }
   return valid;
 }
 
@@ -238,7 +333,22 @@ function validCVV() {
   const valid = val.length === 3 &&
                 !isNaN(val);
   
-  valid ? cvv.style.borderColor = 'rgba(36, 28, 21, 0.3)' : cvv.style.borderColor = 'red';
+  // valid ? cvv.style.borderColor = 'rgba(36, 28, 21, 0.3)' : cvv.style.borderColor = 'red';
+  if (valid) {
+    cvv.style.borderColor = 'rgba(36, 28, 21, 0.3)';
+    cvvLabel.style.color = "rgb(0, 0, 0)";
+    cvvLabel.classList.add('valid');
+    cvvLabel.classList.remove('not-valid');
+    cvvHint.style.color = 'rgba(0, 0, 0, 0.8)';
+    cvvHint.textContent = 'Valid CVV';
+  } else {
+    cvv.style.borderColor = 'red';
+    cvvLabel.style.color = "red";
+    cvvLabel.classList.remove('valid');
+    cvvLabel.classList.add('not-valid');
+    cvvHint.style.color = 'red';
+    cvvHint.textContent = 'CVV must be 3 digits';
+  }
   return valid;
 }
 
@@ -250,7 +360,7 @@ cvv.addEventListener('keyup', e => {
 // Form validation
 const validForm = () => {
   const testArr = [validName(), validMail(), validActivity()];
-  if (paymentMethod.value === 'credit card') {
+  if (paymentMethod.value === 'credit-card') {
     testArr.push(validCC());
     testArr.push(validZip());
     testArr.push(validCVV());
@@ -264,4 +374,4 @@ const validForm = () => {
 document.querySelector('form').addEventListener('submit', e => {
   // e.preventDefault();  
   if (!validForm()) e.preventDefault();
-})
+});
